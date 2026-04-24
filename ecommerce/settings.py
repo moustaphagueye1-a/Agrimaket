fee4a47fce38ad5e7d3d9f9017b174b6b5dbc04c
moustaphagueye1-a <moustapha.gueye1@etu.ussein.edu.sn>
2026-04-24 23:44:34 +0000

    mode production v3

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -4,7 +4,7 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-dev-key')
-DEBUG      = os.environ.get('DEBUG', 'True') == 'True'
+DEBUG      =True
 
 ALLOWED_HOSTS = ['*']
 
