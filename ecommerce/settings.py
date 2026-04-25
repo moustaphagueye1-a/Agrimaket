fc749b6c902e6e85c774bb31f10db22948d9f768
moustaphagueye1-a <moustapha.gueye1@etu.ussein.edu.sn>
2026-04-25 14:59:00 +0000

    fix: PostgreSQL + Cloudinary + MongoDB Atlas v2

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -6,7 +6,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # ── Sécurité ─────────────────────────────────────────────
 SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-dev-key')
-DEBUG = os.environ.get('DEBUG', 'False') == 'True'
+DEBUG =True
 
 ALLOWED_HOSTS = ['*']
 
